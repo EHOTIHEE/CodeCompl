8d02f44753b4c41c5bf31504f05d259d5f79fcf9
ivanlobashev <ivan.lobashev@forgood.ai>
2025-02-28 12:54:34 +0400

    123

--- pythonPart/codeComplNoCodeFilePython.py
+++ pythonPart/codeComplNoCodeFilePython.py
@@ -1,7 +1,5 @@
 from codeComplMathPython import factorial
-# add function to pars json file and return list of dicts 3 case
-
-
+# Recursively flattens a JSON structure into a list of dictionaries
 
 
 
